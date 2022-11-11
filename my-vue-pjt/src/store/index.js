92fccd7ca5531ade15e7990267e09fda6126e010
giokim12 <giokim1011@gmail.com>
2022-11-11 11:29:03 +0900

    .

--- my-vue-pjt/src/store/index.js
+++ my-vue-pjt/src/store/index.js
@@ -6,18 +6,40 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     movieList: [],
+    watchList: [],
   },
   getters: {
   },
   mutations: {
     GET_MOVIE(state, movieList) {
       state.movieList = movieList
+    },
+    CREATE_WATCH(state, watchItem) {
+      state.watchList.push(watchItem)
+    },
+    UPDATE_WATCH_STATUS(state, watchItem) {
+      state.watchList = state.watchList.map((watch) => {
+        if (watch === watchItem) {
+          watch.isSeen = !watch.isSeen
+        }
+        return watch
+      })
     }
   },
   actions: {
     getMovie(context, response) {
       context.commit("GET_MOVIE", response);
     },
+    createWatch(context, watchTitle) {
+      const watchItem = {
+        title : watchTitle,
+        isSeen: false,
+      }
+      context.commit('CREATE_WATCH', watchItem)
+    },
+    updateWatchStatus(context, watchItem) {
+      context.commit('UPDATE_WATCH_STATUS', watchItem)
+    }
   },
   modules: {
   }
