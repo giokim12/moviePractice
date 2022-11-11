901eb464ad37d1ab1365769a7fcbf8c3228a9074
moonthree <fpdhslr0725@gmail.com>
2022-11-11 11:29:04 +0900

    a

--- my-vue-pjt/src/store/index.js
+++ my-vue-pjt/src/store/index.js
@@ -1,24 +1,27 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     movieList: [],
   },
   getters: {
+    randomMovie(state) {
+      const random = Math.floor(Math.random() * state.movieList.length);
+      return state.movieList[random];
+    },
   },
   mutations: {
     GET_MOVIE(state, movieList) {
-      state.movieList = movieList
-    }
+      state.movieList = movieList;
+    },
   },
   actions: {
     getMovie(context, response) {
       context.commit("GET_MOVIE", response);
     },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
